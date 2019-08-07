3e2f6d1230e0b04092adc1fb2c0446b97a7d6aa2
Michael Williams <manicdesigner@gmail.com>
2019-08-07 19:28:08 -0400

    feat(index.js) + h4s w/ querySelectorAll textContent

--- js/index.js
+++ js/index.js
@@ -1,45 +1,45 @@
 const siteContent = {
-  nav: {
-    'nav-item-1': 'Services',
-    'nav-item-2': 'Product',
-    'nav-item-3': 'Vision',
-    'nav-item-4': 'Features',
-    'nav-item-5': 'About',
-    'nav-item-6': 'Contact',
-    'img-src': 'img/logo.png'
-  },
-  cta: {
-    h1: 'DOM Is Awesome',
-    button: 'Get Started',
-    'img-src': 'img/header-img.png'
-  },
-  'main-content': {
-    'features-h4': 'Features',
-    'features-content':
-      'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
-    'about-h4': 'About',
-    'about-content':
-      'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
-    'middle-img-src': 'img/mid-page-accent.jpg',
-    'services-h4': 'Services',
-    'services-content':
-      'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
-    'product-h4': 'Product',
-    'product-content':
-      'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
-    'vision-h4': 'Vision',
-    'vision-content':
-      'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
-  },
-  contact: {
-    'contact-h4': 'Contact',
-    address: '123 Way 456 Street Somewhere, USA',
-    phone: '1 (888) 888-8888',
-    email: 'sales@greatidea.io'
-  },
-  footer: {
-    copyright: 'Copyright Great Idea! 2018'
-  }
+	nav: {
+		'nav-item-1': 'Services',
+		'nav-item-2': 'Product',
+		'nav-item-3': 'Vision',
+		'nav-item-4': 'Features',
+		'nav-item-5': 'About',
+		'nav-item-6': 'Contact',
+		'img-src': 'img/logo.png',
+	},
+	cta: {
+		h1: 'DOM Is Awesome',
+		button: 'Get Started',
+		'img-src': 'img/header-img.png',
+	},
+	'main-content': {
+		'features-h4': 'Features',
+		'features-content':
+			'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+		'about-h4': 'About',
+		'about-content':
+			'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+		'middle-img-src': 'img/mid-page-accent.jpg',
+		'services-h4': 'Services',
+		'services-content':
+			'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+		'product-h4': 'Product',
+		'product-content':
+			'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+		'vision-h4': 'Vision',
+		'vision-content':
+			'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+	},
+	contact: {
+		'contact-h4': 'Contact',
+		address: '123 Way 456 Street Somewhere, USA',
+		phone: '1 (888) 888-8888',
+		email: 'sales@greatidea.io',
+	},
+	footer: {
+		copyright: 'Copyright Great Idea! 2018',
+	},
 };
 
 // Header -------------------------------------------------- //
@@ -47,7 +47,7 @@ const siteContent = {
 const navigationLinks = document.querySelectorAll('nav a');
 
 for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].textContent = siteContent['nav'][`nav-item-${i}`];
+	navigationLinks[i].textContent = siteContent['nav'][`nav-item-${i}`];
 }
 
 let logo = document.getElementById('logo-img');
@@ -66,3 +66,11 @@ ctaButton.textContent = siteContent['cta']['button'];
 
 const ctaImage = document.querySelector('#cta-img');
 ctaImage.setAttribute('src', siteContent['cta']['img-src']);
+
+const mainContentH4 = document.querySelectorAll('.text-content h4');
+
+mainContentH4[0].textContent = siteContent['main-content']['features-h4'];
+mainContentH4[1].textContent = siteContent['main-content']['about-h4'];
+mainContentH4[2].textContent = siteContent['main-content']['services-h4'];
+mainContentH4[3].textContent = siteContent['main-content']['product-h4'];
+mainContentH4[4].textContent = siteContent['main-content']['vision-h4'];
